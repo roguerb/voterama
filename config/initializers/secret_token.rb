553f206322f51b8c46634ae67189c852228397a9
Luke Barbuto <luke.barbuto@gmail.com>
2014-02-04 20:19:40 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voterama::Application.config.secret_key_base = 'd2fb137db2aaebea1f39cfa17dba6b266a2404b1cf65acf7361ceac5b98bfdb7e3b5028f6e1a1fabf789b1a5b8c396ca90bbcc9d59ed44ed2b5a4110136cd963'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Voterama::Application.config.session_store :cookie_store, key: '_voterama_session'
